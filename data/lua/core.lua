025832669800a59ca1ff5f9361481a2f50f808ee
Severin Glöckner <severin.gloeckner@stud.htwk-leipzig.de>
2020-09-19 22:08:10 +0200

    Lua: fix wesnoth.interface.move_unit_fake

--- data/lua/core.lua
+++ data/lua/core.lua
@@ -535,7 +535,7 @@ if wesnoth.kernel_type() == "Game Lua Kernel" then
 		local from_x, from_y = moving_unit.x, moving_unit.y
 
 		wesnoth.interface.scroll_to_hex(from_x, from_y)
-		to_x, to_y = wesnoth.find_vacant_tile(x, y, moving_unit)
+		to_x, to_y = wesnoth.find_vacant_tile(to_x, to_y, moving_unit)
 
 		if to_x < from_x then
 			moving_unit.facing = "sw"
@@ -544,17 +544,17 @@ if wesnoth.kernel_type() == "Game Lua Kernel" then
 		end
 		moving_unit:extract()
 
-		wml_actions.move_unit_fake{
+		wesnoth.wml_actions.move_unit_fake{
 			type      = moving_unit.type,
 			gender    = moving_unit.gender,
-			variation = moving.variation,
+			variation = moving_unit.variation,
 			side      = moving_unit.side,
 			x         = from_x .. ',' .. to_x,
 			y         = from_y .. ',' .. to_y
 		}
 
 		moving_unit:to_map(to_x, to_y)
-		wml_actions.redraw{}
+		wesnoth.wml_actions.redraw{}
 	end
 
 	--[========[Units module]========]
